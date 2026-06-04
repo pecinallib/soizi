42085326157c97eb28dc6ce4d195380900e56f51
pecinallib <pecinallipoker@gmail.com>
2026-06-04 07:52:59 -0300

    chore(pages): remove export de Register do index

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,5 +1,4 @@
 export { Login } from './Login';
-export { Register } from './Register';
 export { Dashboard } from './Dashboard';
 export { Converter } from './Converter';
 export { Glossary } from './Glossary';
